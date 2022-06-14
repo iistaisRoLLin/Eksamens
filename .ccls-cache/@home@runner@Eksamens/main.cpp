43ab394afa6cf85f853bf6d3bc52451fa0506619
Kristers <98739149+iistaisRoLLin@users.noreply.github.com>
2022-06-14 21:25:00 +0000

    Koda pabeigšana, kļūdu labošana

--- .ccls-cache/@home@runner@Eksamens/main.cpp
+++ .ccls-cache/@home@runner@Eksamens/main.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 #include <cctype>
 using namespace std;
-extern  int nep=0;
+int nep=0;
+void beigas(){
+  cout<<"Testā tu atbildēji uz 10 jautājumiem un no tiem Jums bija "<<nep<<" nepareizi";
+  exit(0);
+  }
 void jautajums10(){
    string atbilde;
-  cout<<"int x = 5, y = 3; \n cout << (5 > 3 || 3 < 4); \n Kāds būs iznākums?  \n a) true \n b) false \n c) 3 \n d) 5"; cin>>atbilde;
+  cout<<"int x = 5, y = 3; \n cout << (5 > 3 || 3 < 4); \n Kāds būs iznākums?  \n a) true \n b) false \n c) 3 \n d) 5\n"; cin>>atbilde;
   if(atbilde=="a" || atbilde == "true"){
     beigas();
   }else
+    nep++;
     beigas();
-  nep++;
+  
+  }
 void jautajums9(){
    string atbilde;
-  cout<<"int x = 5, y = 3; \n cout << !(5 < 3 || 3 > 4); \n Kāds būs iznākums?  \n a) true \n b) false \n c) 3 \n d) 5"; cin>>atbilde;
+  cout<<"int x = 5, y = 3; \n cout << !(5 < 3 || 3 > 4); \n Kāds būs iznākums?  \n a) true \n b) false \n c) 3 \n d) 5\n"; cin>>atbilde;
   if(atbilde=="a" || atbilde == "true"){
     jautajums10();
   }else
+    nep++;
     jautajums10();
-  nep++;
+  
   }
 void jautajums8(){
    string atbilde;
-  cout<<"int x = 5, y = 3; \n cout << !(5 > 3 || 3 > 4); \n Kāds būs iznākums?  \n a) true \n b) false \n c) 3 \n d) 5"; cin>>atbilde;
+  cout<<"int x = 5, y = 3; \n cout << !(5 > 3 || 3 > 4); \n Kāds būs iznākums?  \n a) true \n b) false \n c) 3 \n d) 5\n"; cin>>atbilde;
   if(atbilde=="b" || atbilde == "false"){
     jautajums9();
   }else
@@ -31,7 +38,7 @@ void jautajums8(){
 }
 void jautajums7(){
    string atbilde;
-  cout<<"int x = 5, y = 3; \n cout << (5 > 3 || 3 > 4); \n Kāds būs iznākums?  \n a) true \n b) false \n c) 3 \n d) 5"; cin>>atbilde;
+  cout<<"int x = 5, y = 3; \n cout << (5 > 3 || 3 > 4); \n Kāds būs iznākums?  \n a) true \n b) false \n c) 3 \n d) 5\n"; cin>>atbilde;
   if(atbilde=="b" || atbilde == "false"){
     jautajums8();
   }else
@@ -40,7 +47,7 @@ void jautajums7(){
 }
 void jautajums6(){
    string atbilde;
-  cout<<"int x = 5, y = 3; \n cout << (5 > 3 || 3 < 4); \n Kāds būs iznākums?  \n a) true \n b) false \n c) 3 \n d) 5"; cin>>atbilde;
+  cout<<"int x = 5, y = 3; \n cout << (5 > 3 || 3 < 4); \n Kāds būs iznākums?  \n a) true \n b) false \n c) 3 \n d) 5\n"; cin>>atbilde;
   if(atbilde=="a" || atbilde == "true"){
     jautajums7();
   }else
@@ -49,7 +56,7 @@ void jautajums6(){
 }
 void jautajums5(){
   string atbilde;
-  cout<<"int a = 5, b = 10, c = 15 \n if(a < b && b < c) \n Kāds būs iznākums? \n a) true \n b) false \n c) 15 \n d) 5"; cin>>atbilde;
+  cout<<"int a = 5, b = 10, c = 15 \n if(a < b && b < c) \n Kāds būs iznākums? \n a) true \n b) false \n c) 15 \n d) 5\n"; cin>>atbilde;
   if(atbilde=="a" || atbilde == "true"){
     jautajums6();
   }else
@@ -59,7 +66,7 @@ void jautajums5(){
 
 void jautajums4(){
  string atbilde;
-  cout<<"Ko nozīmē simbols .!=. ? \n a) Patiess \n b) Aplams \n c) Mazāks \n d) Nepatiess"; cin>>atbilde;
+  cout<<"Ko nozīmē simbols .!=. ? \n a) Patiess \n b) Aplams \n c) Mazāks \n d) Nepatiess\n"; cin>>atbilde;
   if(atbilde=="d" || atbilde == "Nepatiess"){
     jautajums5();
   }else
@@ -69,7 +76,7 @@ void jautajums4(){
 
 void jautajums3(){
   string atbilde;
-  cout<<"Ko nozīmē simbols .=. ? \n a) Vienāds \n b) Lielāks \n c) Mazāks \n d) Nepatiess"; cin>>atbilde;
+  cout<<"Ko nozīmē simbols .=. ? \n a) Vienāds \n b) Lielāks \n c) Mazāks \n d) Nepatiess\n"; cin>>atbilde;
   if(atbilde=="a" || atbilde == "Vienāds"){
     jautajums4();
   }else
@@ -79,7 +86,7 @@ void jautajums3(){
 
 void jautajums2(){
   string atbilde;
-  cout<<"Ko nozīmē simbols .>. ? \n a) Vienāds \n b) Lielāks \n c) Mazāks \n d) Nepatiess"; cin>>atbilde;
+  cout<<"Ko nozīmē simbols .>. ? \n a) Vienāds \n b) Lielāks \n c) Mazāks \n d) Nepatiess\n"; cin>>atbilde;
   if(atbilde=="b" || atbilde == "Lielāks"){
     jautajums3();
   }else
@@ -88,7 +95,7 @@ void jautajums2(){
 }
 void jautajums1(){
    string atbilde;
-cout<<"Ko nozīmē simbols .<. ? \n a) Vienāds \n b) Lielāks \n c) Mazāks \n d) Nepatiess"; cin>>atbilde;
+cout<<"Ko nozīmē simbols .<. ? \n a) Vienāds \n b) Lielāks \n c) Mazāks \n d) Nepatiess\n"; cin>>atbilde;
   if(atbilde=="c" || atbilde == "mazāks"){
     jautajums2();
   }else
@@ -108,15 +115,8 @@ int main() {
     cin>>izvele;
   switch(izvele){
     case 1: jautajums1();
-    case 2: beigas();
-    case 3: beigt();
-    case 4: jautajums5();
+    case 3: jautajums9();
 
-
-
-
-    cout<<"vai velies turpinat (j/n)";
-  cin>>kkas;
       }
   }while(kkas == "j");
 }
